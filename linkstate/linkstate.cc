6b87828eb177cf06c0f5951f66a54dd5920a1f04
7hong13 <ledi000@naver.com>
2021-05-14 23:37:15 +0900

    Initial setup

---
+++ linkstate/linkstate.cc
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void exitArgsErr() {
+    fprintf(stderr, "usage: linkstate topologyfile messagesfile changesfile\n");
+    exit(1);
+}
+
+void exitInputErr() {
+    fprintf(stderr, "Error: open input file.\n");
+    exit(1);
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        exitArgsErr();
+    }
+
+    FILE *fp;
+    int nodeNum;
+
+    fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        exitInputErr();
+    }
+
+    printf("Complete. Output file written to output_ls.txt.");
+
+    return 0;
+}
